2a3010f094318ae5a2c463cf110fac221626c71b
ActiveZ <activez.git@gmail.com>
2020-02-10 16:46:57 +0100

    Exo2 debut

---
+++ src/Exercice2/Compteur3.java
@@ -0,0 +1,23 @@
+package Exercice2;
+
+public class Compteur3 {
+    private int pas=0; //attributs
+
+    public Compteur3(int pas) { //constructeur
+        this.pas=pas;
+    }
+
+    public int getPas() {
+        return pas;
+    }
+
+    public void setPas(int pas) {
+        this.pas = pas;
+    }
+
+    public void getNumber() { //méthode
+        System.out.print("Série de " + pas + ": ");
+        for (int i = 1; i <= 10; i++) System.out.print(pas*i + ", ");
+        System.out.println();
+    }
+}
---
+++ src/Exercice2/Launch.java
@@ -0,0 +1,8 @@
+package Exercice2;
+
+public class Launch {
+    public static void main(String[] args) {
+        Compteur3 cpt = new Compteur3(2);
+        cpt.getNumber();
+    }
+}
