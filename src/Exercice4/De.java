69bb16bcc4d5ff7d4506cbbadbc6db7a346cdc49
ActiveZ <activez.git@gmail.com>
2020-02-10 23:15:49 +0100

    exercice4

---
+++ src/Exercice4/De.java
@@ -0,0 +1,20 @@
+package Exercice4;
+
+import java.util.Random;
+
+public class De {
+    Random rd = new Random();
+    private  int nbFace=6;
+
+    public De(int nbFace) { // constructeur pour fixer le nb de face du dé
+        this.nbFace = nbFace;
+    }
+
+    public De() { // constructeur par défaut: dé 6 faces
+        this.nbFace = 6;
+    }
+
+    public int lancer() {
+        return (rd.nextInt(nbFace) + 1);
+    }
+}
---
+++ src/Exercice4/Launch.java
@@ -0,0 +1,11 @@
+package Exercice4;
+
+public class Launch {
+    public static void main(String[] args) {
+        int test=0;
+        De de = new De();
+        System.out.println("Dé: " + de.lancer());
+//        for (int i=0; i<100000; i++) test += de.lancer();
+//        System.out.println("test: " + test);
+    }
+}
