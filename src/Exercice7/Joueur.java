de1851dbd458824db2b25876b705736bdcbc594d
ActiveZ <activez.git@gmail.com>
2020-02-14 00:09:10 +0100

    nettoyage de la classe joueur

--- src/Exercice7/Joueur.java
+++ src/Exercice7/Joueur.java
@@ -1,21 +1,12 @@
 package Exercice7;
 
 public class Joueur {
-    private int mise = 0;
     public String  nom = "";
     private De de = new De();
 
     public Joueur(String nom) {
         this.nom = nom;
-    }
-
-    public int getMise() {
-        return mise;
-    }
-
-    public void setMise(int mise) {
-        this.mise = mise;
-    }
+    } //constructeur
 
     public int lanceDe () {
         return de.lancer();
