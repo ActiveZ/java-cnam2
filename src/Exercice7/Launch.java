3fe21aa5ca9f15397a6c94d9d194431cba578a58
ActiveZ <activez.git@gmail.com>
2020-02-13 23:57:45 +0100

    jeu du 36 fini

--- src/Exercice7/Launch.java
+++ src/Exercice7/Launch.java
@@ -21,7 +21,7 @@ public class Launch {
             nbJoueur = sc.nextInt(); // A faire: vérifier validité du paramètre entrant (try/catch)
         } while (nbJoueur < 2 || nbJoueur > 6);
 
-        //instanciation des joueurs et ajout dans la collection
+        //instanciation des joueurs et ajout dans la collection listeJoueur
         for (int i=0; i < nbJoueur; i++) {
             sc.nextLine(); // pour vider le scanner
             System.out.println("Quel est le nom du joueur " + i + " ?");
@@ -31,9 +31,26 @@ public class Launch {
             listeJoueur.add(joueur);
         }
 
+        // le jeu
+        int i = 0;
+        int tirage = 0;
+        int tirageSum = 0;
 
+        do {
+            tirage = listeJoueur.get(i).lanceDe();
+            System.out.println("\nLe joueur " + listeJoueur.get(i).nom + " lance le dé et fait " + tirage);
 
-        for  (Joueur j : listeJoueur) {System.out.println(j.nom + " lance et fait: " + j.lanceDe());}
+            if (tirage + tirageSum > 36) {
+                System.out.println("Total dépassé, " + listeJoueur.get(i).nom + " est éliminé !");
+                listeJoueur.remove(i); // il dégage de la collection
+            } else {
+                tirageSum += tirage;
+                System.out.println("La somme des dés est de " + tirageSum);
+                if (tirageSum != 36) {i++;} // passage au joueur suivant si aucun gagnant
+            }
+            if (i==listeJoueur.size()) {i=0;} // retour au début de la collection si arrivé à la fin
+        } while ((listeJoueur.size() > 1) && (tirageSum != 36)); 
 
+        System.out.println("\nLe joueur " + listeJoueur.get(i).nom + " remporte le pot de " + pot + " cacahuètes !!!");
     }
 }
